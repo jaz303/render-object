6e99935b89203fe1494a636d09364b7449ac0d1a
Jason Frame <jason@onehackoranother.com>
2015-07-16 23:55:46 +0100

    tabs -> spaces

--- demo/bundle.js
+++ demo/bundle.js
@@ -2,20 +2,20 @@
 var render = require('../');
 
 window.init = function() {
-	var el = render({
-		a: [1,2,3,4,{foo: 'bar', baz: 'bleem'}],
-		b: 2,
-		c: {
-			foo: "this is a string",
-			bar: true,
-			'null': null,
-			'void': void 0
-		},
-		validUsername: /^[a-z][a-z]*$/i,
-		now: new Date()
-	});
-	console.log(el);
-	document.body.appendChild(el);
+    var el = render({
+        a: [1,2,3,4,{foo: 'bar', baz: 'bleem'}],
+        b: 2,
+        c: {
+            foo: "this is a string",
+            bar: true,
+            'null': null,
+            'void': void 0
+        },
+        validUsername: /^[a-z][a-z]*$/i,
+        now: new Date()
+    });
+    console.log(el);
+    document.body.appendChild(el);
 }
 },{"../":"/Users/jason/dev/projects/render-object/index.js"}],"/Users/jason/dev/projects/render-object/index.js":[function(require,module,exports){
 var isArray = require('is-array');
@@ -25,124 +25,124 @@ var rules = [];
 var primitives = {};
 
 module.exports = function(thing) {
-	var wrapper = document.createElement('div');
-	wrapper.className = classPrefix;
-	wrapper.appendChild(render(thing));
-	return wrapper;
+    var wrapper = document.createElement('div');
+    wrapper.className = classPrefix;
+    wrapper.appendChild(render(thing));
+    return wrapper;
 }
 
 function render(thing) {
-	if (thing === null) {
-		return primitives['null']();
-	} else if (typeof thing !== 'object') {
-		return primitives[typeof thing](thing);
-	} else if (isArray(thing)) {
-		return renderArray(thing);
-	} else {
-		for (var i = 0; i < rules.length; ++i) {
-			if (rules[i].test(thing)) {
-				return rules[i].render(thing);
-			}
-		}
-		return renderObject(thing);
-	}
+    if (thing === null) {
+        return primitives['null']();
+    } else if (typeof thing !== 'object') {
+        return primitives[typeof thing](thing);
+    } else if (isArray(thing)) {
+        return renderArray(thing);
+    } else {
+        for (var i = 0; i < rules.length; ++i) {
+            if (rules[i].test(thing)) {
+                return rules[i].render(thing);
+            }
+        }
+        return renderObject(thing);
+    }
 }
 
 function renderPrimitive(str, classSuffix) {
-	var el = document.createElement('span');
-	el.className = classPrefix + '-' + classSuffix;
-	el.textContent = str;
-	return el;
+    var el = document.createElement('span');
+    el.className = classPrefix + '-' + classSuffix;
+    el.textContent = str;
+    return el;
 }
 
 function renderCollapsible(header, className, items) {
-	var el = document.createElement('div');
-	el.className = classPrefix + '-' + className;
-	var actuator = document.createElement('a');
-	actuator.className = classPrefix + '-expander';
-	actuator.textContent = header;
-	var expanded = false;
-	actuator.onclick = function() {
-		expanded = !expanded;
-		if (expanded) {
-			el.className += ' isExpanded ';
-		} else {
-			el.className = el.className.replace(/\s*isExpanded\s*/g, '');
-		}
-	}
-	items.className = classPrefix + '-' + className + 'Items';
-	el.appendChild(actuator);
-	el.appendChild(items);
-	return el;
+    var el = document.createElement('div');
+    el.className = classPrefix + '-' + className;
+    var actuator = document.createElement('a');
+    actuator.className = classPrefix + '-expander';
+    actuator.textContent = header;
+    var expanded = false;
+    actuator.onclick = function() {
+        expanded = !expanded;
+        if (expanded) {
+            el.className += ' isExpanded ';
+        } else {
+            el.className = el.className.replace(/\s*isExpanded\s*/g, '');
+        }
+    }
+    items.className = classPrefix + '-' + className + 'Items';
+    el.appendChild(actuator);
+    el.appendChild(items);
+    return el;
 }
 
 function renderArray(ary) {
-	var el = document.createElement('ol');
-	el.setAttribute('start', 0);
-	ary.forEach(function(val) {
-		var wrap = document.createElement('li');
-		wrap.className = classPrefix + '-ArrayItem';
-		wrap.appendChild(render(val));
-		el.appendChild(wrap);
-	});
-	return renderCollapsible('Array (' + ary.length + ')', 'Array', el);
+    var el = document.createElement('ol');
+    el.setAttribute('start', 0);
+    ary.forEach(function(val) {
+        var wrap = document.createElement('li');
+        wrap.className = classPrefix + '-ArrayItem';
+        wrap.appendChild(render(val));
+        el.appendChild(wrap);
+    });
+    return renderCollapsible('Array (' + ary.length + ')', 'Array', el);
 }
 
 function renderObject(obj) {
-	var pairs = document.createElement('dl');
-	for (var k in obj) {
-		var dt = document.createElement('dt');
-		dt.className = classPrefix + '-ObjectKey';
-		dt.textContent = k;
-		var dd = document.createElement('dd');
-		dd.className = classPrefix + '-ObjectValue';
-		dd.appendChild(render(obj[k]));
-		pairs.appendChild(dt);
-		pairs.appendChild(dd);
-	}
-	return renderCollapsible('Object', 'Object', pairs);
+    var pairs = document.createElement('dl');
+    for (var k in obj) {
+        var dt = document.createElement('dt');
+        dt.className = classPrefix + '-ObjectKey';
+        dt.textContent = k;
+        var dd = document.createElement('dd');
+        dd.className = classPrefix + '-ObjectValue';
+        dd.appendChild(render(obj[k]));
+        pairs.appendChild(dt);
+        pairs.appendChild(dd);
+    }
+    return renderCollapsible('Object', 'Object', pairs);
 }
 
 function registerObjectRenderer(test, renderer) {
-	rules.push({test: test, render: renderer});
+    rules.push({test: test, render: renderer});
 }
 
 function registerClassRenderer(klass, renderer) {
-	registerObjectRenderer(function(thing) {
-		return thing instanceof klass;
-	}, renderer);
+    registerObjectRenderer(function(thing) {
+        return thing instanceof klass;
+    }, renderer);
 }
 
 function registerPrimitiveRenderer(prim, renderer) {
-	primitives[prim] = renderer;
+    primitives[prim] = renderer;
 }
 
 registerClassRenderer(RegExp, function(regexp) {
-	return renderPrimitive(regexp.toString(), 'RegExp');
+    return renderPrimitive(regexp.toString(), 'RegExp');
 });
 
 registerClassRenderer(Date, function(date) {
-	return renderPrimitive(date.toString(), 'Date');
+    return renderPrimitive(date.toString(), 'Date');
 });
 
 registerPrimitiveRenderer('boolean', function(val) {
-	return renderPrimitive(val ? 'true' : 'false', 'Boolean');
+    return renderPrimitive(val ? 'true' : 'false', 'Boolean');
 });
 
 registerPrimitiveRenderer('null', function() {
-	return renderPrimitive('null', 'Null');
+    return renderPrimitive('null', 'Null');
 });
 
 registerPrimitiveRenderer('number', function(val) {
-	return renderPrimitive('' + val, 'Number');
+    return renderPrimitive('' + val, 'Number');
 })
 
 registerPrimitiveRenderer('string', function(val) {
-	return renderPrimitive(JSON.stringify(val), 'String');
+    return renderPrimitive(JSON.stringify(val), 'String');
 });
 
 registerPrimitiveRenderer('undefined', function() {
-	return renderPrimitive('undefined', 'Undefined');
+    return renderPrimitive('undefined', 'Undefined');
 });
 },{"is-array":"/Users/jason/dev/projects/render-object/node_modules/is-array/index.js"}],"/Users/jason/dev/projects/render-object/node_modules/is-array/index.js":[function(require,module,exports){
 
--- demo/main.js
+++ demo/main.js
@@ -1,18 +1,18 @@
 var render = require('../');
 
 window.init = function() {
-	var el = render({
-		a: [1,2,3,4,{foo: 'bar', baz: 'bleem'}],
-		b: 2,
-		c: {
-			foo: "this is a string",
-			bar: true,
-			'null': null,
-			'void': void 0
-		},
-		validUsername: /^[a-z][a-z]*$/i,
-		now: new Date()
-	});
-	console.log(el);
-	document.body.appendChild(el);
+    var el = render({
+        a: [1,2,3,4,{foo: 'bar', baz: 'bleem'}],
+        b: 2,
+        c: {
+            foo: "this is a string",
+            bar: true,
+            'null': null,
+            'void': void 0
+        },
+        validUsername: /^[a-z][a-z]*$/i,
+        now: new Date()
+    });
+    console.log(el);
+    document.body.appendChild(el);
 }
